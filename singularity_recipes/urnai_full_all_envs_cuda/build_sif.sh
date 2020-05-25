a1203f8f223bdb9dd4922775b2ee1d27924b544d
marcocspc <marcocspc@hotmail.com>
2020-05-25 15:41:56 -0300

    urnai full singularity recipe is ready

--- singularity_recipes/urnai_full_all_envs_cuda/build_sif.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo singularity build urnai_full_singularity_container.sif urnai_full.simg
--- singularity_recipes/urnai_full_all_envs_cuda/build_sif_macos_docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Tenha certeza de que está na pasta do arquivo .simg ao executar este script, pois ele usa o pwd para localizar os arquivos."
-
-dir=$(pwd)
-
-docker run -e TZ=America/Fortaleza --privileged -v $dir:/mnt singularity build /mnt/urnai_full_singularity_container.sif /mnt/urnai_full.simg
