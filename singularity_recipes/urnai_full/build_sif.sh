d5594c3b424a14c743090405707da21d88a6e15f
marcocspc <marcocspc@hotmail.com>
2020-06-12 15:19:18 -0300

    build sif modified

--- singularity_recipes/urnai_full/build_sif.sh
+++ singularity_recipes/urnai_full/build_sif.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-sudo singularity build urnai_full_singularity_container.sif urnai_full.simg
+echo "Tenha certeza de que está na pasta do arquivo .simg ao executar este script, pois ele usa o pwd para localizar os arquivos."
+
+dir=$(pwd)
+
+sudo singularity build "$dir/urnai_full_singularity_container.sif" "$dir/urnai_full.simg"
