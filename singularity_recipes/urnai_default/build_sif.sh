c8db2b1803fd100fbb25ca14b82e5467eb59779d
marcocspc <you@example.com>
2021-08-16 20:49:48 -0300

    default sing urnai build updated

--- singularity_recipes/urnai_default/build_sif.sh
+++ singularity_recipes/urnai_default/build_sif.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-sudo singularity build urnai_default_singularity_container.sif urnai_default.simg
+echo "Tenha certeza de que está na pasta do arquivo .simg ao executar este script, pois ele usa o pwd para localizar os arquivos."
+
+dir=$(pwd)
+
+sudo singularity build "$dir/urnai_default_singularity_container_$(date +%F).sif" "$dir/urnai_default.simg"
---
+++ singularity_recipes/urnai_default/build_sif_macos_docker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Tenha certeza de que está na pasta do arquivo .simg ao executar este script, pois ele usa o pwd para localizar os arquivos."
+
+dir=$(pwd)
+
+docker run -e TZ=America/Fortaleza --privileged -v $dir:/mnt -e SINGULARITY_CACHEDIR=/mnt -e SINGULARITY_TMPDIR=/mnt singularityware/singularity build /mnt/urnai_full_singularity_container.sif /mnt/urnai_full.simg
